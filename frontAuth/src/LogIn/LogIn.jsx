bd55d284deb081c8afe5f0c5fe6d60a77f965d14
Fabio-Alfredo <00210121@uca.edu.sv>
2024-04-18 22:26:13 -0600

    add: query for login complete

--- frontAuth/src/LogIn/LogIn.jsx
+++ frontAuth/src/LogIn/LogIn.jsx
@@ -1,6 +1,25 @@
+import { login } from "../service/service";
 import InputField from "./InputFiled";
 
 const LogIn = () => {
+
+    const handleSubmint = async (event) => {
+        event.preventDefault();
+
+        const formData={
+            userName:event.target.value,
+            password: event.target.value
+        }
+
+        try {
+            const userData = await login(formData);
+            console.log("Usuario autenticado:", userData);
+        } catch (error) {
+            console.error("Error de autenticación:", error.message);
+        }
+
+    }
+
     return (
         <>
             <div className="flex justify-center items-center w-screen h-screen bg-gradient-to-l from-stone-950 via-stone-900 to-stone-600 bg-300% animate-gradient">
@@ -8,9 +27,9 @@ const LogIn = () => {
                     <p className="font-Bebas text-4xl">
                         Iniciar Sesion
                     </p>
-                    <form className="flex flex-col gap-5 w-4/6 items-center">
-                        <InputField nameField="Username" type="text"/>
-                        <InputField nameField="Password" type="password"/>
+                    <form className="flex flex-col gap-5 w-4/6 items-center" onSubmit={handleSubmint}>
+                        <InputField nameField="Username" type="text" />
+                        <InputField nameField="Password" type="password" />
                         <input className="bg-black w-3/6 p-4 text-white font-Roboto self-end rounded-xl mt-6" type="submit" value="Iniciar Sesión" />
                     </form>
                 </div>
