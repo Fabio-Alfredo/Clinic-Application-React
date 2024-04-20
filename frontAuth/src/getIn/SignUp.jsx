727b2493d6e399ec9082a00c31a2cb1b28d396cd
Nietoof6 <00087419@uca.edu.sv>
2024-04-19 20:08:27 -0600

    add: fixing navlinks

--- frontAuth/src/getIn/SignUp.jsx
+++ frontAuth/src/getIn/SignUp.jsx
@@ -14,9 +14,9 @@ const SignUp = () => {
                         Sign Up
                     </p>
                     <form className="flex flex-col gap-5 w-4/6 items-center">
-                        <InputField nameField="Username" type="text" />
-                        <InputField nameField="Email" type="email" />
-                        <InputField nameField="Password" type="password" />
+                        <InputField nameField="Username" type="text" placeH="e.g. NimbusX"/>
+                        <InputField nameField="Email" type="email" placeH="e.g. example@gmail.com"/>
+                        <InputField nameField="Password" type="password"/>
                         <InputField nameField="Confirm password" type="password" />
                         <input className="bg-black min-w-[50%] p-4 text-white font-Roboto self-end rounded-xl mt-6 hover:bg-slate-100/70 hover:text-black transition ease-in-out duration-200 hover:ring-2 hover:ring-white" type="submit" value="Sign Up" />
                     </form>
