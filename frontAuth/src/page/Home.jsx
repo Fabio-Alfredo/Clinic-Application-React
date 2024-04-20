60f21d8fd50dc69ca7db5386a53375589046ed87
Fabio-Alfredo <00210121@uca.edu.sv>
2024-04-20 12:34:40 -0600

    add: logout

--- frontAuth/src/page/Home.jsx
+++ frontAuth/src/page/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+    const navigate = useNavigate()
+
+    const handleLogout=()=>{
+        localStorage.removeItem("isAuthenticated")
+        navigate('/')
+    }
+
     return (
         <div>
             <h1 className='text-center font-bold'>WELCOME</h1>
+            <button className="bg-black min-w-[50%] p-4 text-white font-Roboto self-end rounded-xl mt-6 hover:bg-slate-100/70 hover:text-black transition ease-in-out duration-200 hover:ring-2 hover:ring-black"  onClick={handleLogout} type="submit" value="Sign In" >Logout</button>
         </div>
     );
 };
