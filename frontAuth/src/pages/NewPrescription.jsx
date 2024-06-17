611eb350d7e8d85724112ec51f6f4a6d04bca39e
Nietoof6 <00087419@uca.edu.sv>
2024-06-16 23:17:06 -0600

    add: adding services to form new prescription

--- frontAuth/src/pages/NewPrescription.jsx
+++ frontAuth/src/pages/NewPrescription.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navigation from '../components/Navigation';
-import { getfinishedAppointments } from '../service/service';
+import { getfinishedAppointments, createPrescription } from '../service/service';
 import { IoAlertCircleOutline } from "react-icons/io5";
 import Swal from 'sweetalert2'
 import Finished from '../components/appointmentsFinished/Finished';
@@ -9,16 +10,19 @@ import { useForm } from '../hooks/useForm';
 
 const NewPrescription = () => {
 
+    const navigation = useNavigate()
     const [appointments, setAppointments] = useState([])
-    const [Appoint, setAppoint] = useState( {name: '', reason: ''})
+    const [Appoint, setAppoint] = useState({ id: '', name: '', reason: '' })
 
-    const { name, reason, appointment, prescription, dosage, medicines } = useForm(
+    const { appointment, d_finalization, dosage, medicine, InputChange, resetForm } = useForm(
         {
-            name: '',
-            reason: '',
-            appointments: '',
-            prescription: [{ dosage: '', medicines: '' }]
+            medicine: '',
+            dosage: '',
+            d_finalization: '',
+            appointment: Appoint.id
         })
+
+    console.log(Appoint.id, Appoint.name, Appoint.reason, appointment, d_finalization, dosage, medicine);
 
     const fetchAppointments = async () => {
         try {
@@ -36,6 +40,35 @@ const NewPrescription = () => {
 
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const formValues = {
+            medicine: medicine,
+            dosage: dosage,
+            d_finalization: d_finalization,
+            appointment: Appoint.id
+        }
+        try {
+            const res = await createPrescription(formValues);
+            Swal.fire({
+                title: "Exitoso!",
+                text: `${res.message}`,
+                icon: "success",
+                showConfirmButton: false,
+                timer: 1500
+            }).then(() => {
+                navigation('/Home')
+            })
+        }
+        catch (error) {
+            Swal.fire({
+                title: "Error!",
+                text: `${error.response.data.message}`,
+                icon: "error",
+            })
+        }
+    }
+
     useEffect(() => {
         fetchAppointments()
     }, [])
@@ -51,8 +84,8 @@ const NewPrescription = () => {
                             <div className='overflow-y-auto overflow-x-hidden w-24rem h-[35vh] md:h-[40vh] px-4 items-center border-solid border-black border-4'>
                                 {
                                     appointments.map(appointment => (
-                                        <div key={appointment.id} onClick={() => 
-                                            setAppoint({ ...Appoint, name: appointment.name, reason: appointment.reason})
+                                        <div key={appointment.id} onClick={() =>
+                                            setAppoint({ ...Appoint, id: appointment.id, name: appointment.name, reason: appointment.reason })
                                         }>
                                             <Finished key={appointment.id} name={appointment.name} reason={appointment.reason} />
                                         </div>
@@ -66,16 +99,18 @@ const NewPrescription = () => {
                         )}
                     </div>
                     <div className='flex w-[90rem] h-[35vh] md:h-[50vh] justify-center items-center'>
-                        <form className='flex w-full h-full flex-wrap gap-2 justify-center'>
+                        <form className='flex w-full h-full flex-wrap gap-2 justify-center' onSubmit={handleSubmit}>
                             <span className="w-1/5 text-lg font-bold">Paciente:</span>
                             <p className='w-3/5'>{Appoint.name}</p>
                             <span className="w-1/5 text-lg font-bold">Razón:</span>
-                            <p className='w-3/5'>{ Appoint.reason }</p>
-                            <span className="w-1/5 text-lg font-bold">Dosis:</span>
-                            <textarea rows={1} className='w-3/5 p-1 rounded border-2' />
+                            <p className='w-3/5'>{Appoint.reason}</p>
                             <span className="w-1/5 text-lg font-bold">Medicina:</span>
-                            <textarea rows={3} className='w-3/5 p-1 rounded border-2' />
-                            <input type="submit" value="Agregar " className="w-1/2 p-2 place-self-end bg-black text-white rounded-xl mt-2 hover:bg-slate-100 hover:text-black transition ease-in-out duration-200 hover:ring-2 hover:ring-black" />
+                            <input className='w-3/5 h-10 p-1 rounded border-2' name='medicine' value={medicine} onChange={InputChange} />
+                            <span className="w-1/5 text-lg font-bold">Dosis:</span>
+                            <input className='w-3/5 h-10 p-1 rounded border-2' name='dosage' value={dosage} onChange={InputChange} />
+                            <span className="w-1/5 text-lg font-bold">Ultima dosis:</span>
+                            <input type='date' name='d_finalization' value={d_finalization} onChange={InputChange} className='w-3/5 h-10 p-1 rounded border-2' />
+                            <input type="submit" value="Recetar" className="w-1/2 p-2 place-self-end bg-black text-white rounded-xl mt-2 hover:bg-slate-100 hover:text-black transition ease-in-out duration-200 hover:ring-2 hover:ring-black" />
                         </form>
                     </div>
                 </div>
