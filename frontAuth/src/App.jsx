abc29484779b15005cd0471d9a3a33a3527f5699
Fabio-Alfredo <00210121@uca.edu.sv>
2024-06-16 22:27:01 -0600

    update: validation roles

--- frontAuth/src/App.jsx
+++ frontAuth/src/App.jsx
@@ -21,9 +21,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/SignUp" element={<SignUp />} />
-          <Route path="/create/historic" element={<CreateHistoric/>} />
 
-          <Route element={<ProtectedRoute canActivate={token} redirectPath="/Home"  />} >
+          <Route element={<ProtectedRoute canActivate={token} redirectPath="/Home" />} >
             <Route path="/Home" element={<Home />} />
           </Route>
 
@@ -33,18 +32,23 @@ const App = () => {
           </Route>
 
           {/* ruta 2 */}
-          <Route element={<ProtectedRoute canActivate={token} RequiredRoles={['DCTR', 'ASST']} userRoles={roles} redirectPath="/Home" />} >
+          <Route element={<ProtectedRoute canActivate={token} RequiredRoles={['ASST']} userRoles={roles} redirectPath="/Home" />} >
             <Route path="/approved/appointent" element={<ApprovedAppointment />} />
           </Route>
 
           {/* route 3 */}
-          <Route element={<ProtectedRoute canActivate={token} redirectPath="/Home" />} >
+          <Route element={<ProtectedRoute canActivate={token} RequiredRoles={['PCTE']} userRoles={roles} redirectPath="/Home" />} >
             <Route path="/appointments" element={<ListPacients />} />
           </Route>
- 
+
           {/* ruta 4 */}
           <Route element={<ProtectedRoute canActivate={token} RequiredRoles={['DCTR']} userRoles={roles} redirectPath="/Home" />} >
             <Route path="/schedule" element={<ListScheduleAppointmets />} />
+          </Route>
+
+          {/* ruta 7 */}
+          <Route element={<ProtectedRoute canActivate={token} RequiredRoles={['DCTR', 'ASST']} userRoles={roles} redirectPath="/Home" />} >
+            <Route path="/create/historic" element={<CreateHistoric />} />
           </Route>
         </Routes>
       </BrowserRouter>
