d0c6481d47a1c57c2582413a11675cd72b837189
Nietoof6 <00087419@uca.edu.sv>
2024-06-17 01:35:09 -0600

    add: adding close appointments page

--- frontAuth/src/pages/ChangeRole.jsx
+++ frontAuth/src/pages/ChangeRole.jsx
@@ -9,7 +9,7 @@ const ChangeRole = () => {
 
     const navigation = useNavigate()
 
-    const { email, idRole, InputChange, resetForm} = useForm({
+    const { email, idRole, InputChange, resetForm } = useForm({
         idRole: '',
         email: ''
     })
@@ -20,7 +20,7 @@ const ChangeRole = () => {
             idRole: idRole,
             email: email
         }
-        try{
+        try {
             const res = await updateRole(formValues);
             Swal.fire({
                 title: "Exitoso!",
@@ -33,7 +33,7 @@ const ChangeRole = () => {
                 navigation('/Home')
             })
         }
-        catch(error){
+        catch (error) {
             Swal.fire({
                 title: "Error!",
                 text: `${error.response.data.message}`,
@@ -44,35 +44,33 @@ const ChangeRole = () => {
 
 
     return (
-        <>
-            <div className='flex items-center justify-center w-full bg-blue-400 px-6  h-screen'>
-                <div className='p-4 sm:p-8 shadow-2xl rounded-3xl bg-white h-fit w-[60rem]' >
-                    <Navigation title={"Citas medicas"} />
-                    <hr className='h-0.5 bg-black mb-6 mx-4' />
-                    <div className='flex w-full h-full'>
-                        <div className='w-full h-full flex justify-center'>
-                            <form className='flex flex-col gap-4 w-3/5 h-[25rem] items-center justify-start' onSubmit={handleSubmit}>
-                                <div className='flex flex-col w-full'>
-                                    <span className='text-lg font-semibold'>Correo electronico: </span>
-                                    <input type='email' id='email' name='email' value={email} onChange={InputChange} className='border-2 rounded-md p-2' />
-                                </div>
-                                <div className='flex flex-col w-full'>
-                                    <span className='text-lg font-semibold'>Rol:</span>
-                                    <select id='rol' name='idRole' value={idRole} onChange={InputChange} className='border-2 border-black rounded-md p-2'>
-                                        <option value=''>Seleccione un rol</option>
-                                        <option value='ADMN'>ADMINISTRADOR</option>
-                                        <option value='DCTR'>DOCTOR</option>
-                                        <option value='ASST'>ASSITENTE</option>
-                                    </select>
-                                </div>
-                                <button type='submit' className='bg-black text-white rounded-md p-2 w-1/5 mt-4 justify-self-end'>Actualizar</button>
-                            </form>
-                        </div>
+        <div className='flex items-center justify-center w-full bg-blue-400 px-6  h-screen'>
+            <div className='p-4 sm:p-8 shadow-2xl rounded-3xl bg-white h-fit w-[60rem]' >
+                <Navigation title={"Citas medicas"} />
+                <hr className='h-0.5 bg-black mb-6 mx-4' />
+                <div className='flex w-full h-full'>
+                    <div className='w-full h-full flex justify-center'>
+                        <form className='flex flex-col gap-4 w-3/5 h-[25rem] items-center justify-start' onSubmit={handleSubmit}>
+                            <div className='flex flex-col w-full'>
+                                <span className='text-lg font-semibold'>Correo electronico: </span>
+                                <input type='email' id='email' name='email' value={email} onChange={InputChange} className='border-2 rounded-md p-2' />
+                            </div>
+                            <div className='flex flex-col w-full'>
+                                <span className='text-lg font-semibold'>Rol:</span>
+                                <select id='rol' name='idRole' value={idRole} onChange={InputChange} className='border-2 border-black rounded-md p-2'>
+                                    <option value=''>Seleccione un rol</option>
+                                    <option value='ADMN'>ADMINISTRADOR</option>
+                                    <option value='DCTR'>DOCTOR</option>
+                                    <option value='ASST'>ASSITENTE</option>
+                                </select>
+                            </div>
+                            <button type='submit' className='bg-black text-white rounded-md p-2 w-1/5 mt-4 justify-self-end'>Actualizar</button>
+                        </form>
                     </div>
-                    <hr className='h-0.5 bg-black my-8 mx-4' />
                 </div>
+                <hr className='h-0.5 bg-black my-8 mx-4' />
             </div>
-        </>
+        </div>
     );
 };
 
--- frontAuth/src/pages/CloseAppointment.jsx
+++ frontAuth/src/pages/CloseAppointment.jsx
@@ -1,63 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import Navigation from '../components/Navigation';
-import { getfinishedAppointments, createPrescription } from '../service/service';
 import { IoAlertCircleOutline } from "react-icons/io5";
-import Swal from 'sweetalert2'
-import Finished from '../components/appointmentsFinished/Finished';
-import { useForm } from '../hooks/useForm';
+import { useState, useEffect } from 'react';
+import { getApprovedAppointments } from '../service/service';
 
+const CloseAppointment = () => {
 
-const NewPrescription = () => {
-
-    const navigation = useNavigate()
     const [appointments, setAppointments] = useState([])
     const [Appoint, setAppoint] = useState({ id: '', name: '', reason: '' })
 
-    const { appointment, d_finalization, dosage, medicine, InputChange, resetForm } = useForm(
-        {
-            medicine: '',
-            dosage: '',
-            d_finalization: '',
-            appointment: Appoint.id
-        })
-
     const fetchAppointments = async () => {
         try {
-            const res = await getfinishedAppointments()
+            const res = await getApprovedAppointments()
             setAppointments(res.data)
 
         } catch (error) {
-            Swal.fire({
-                title: "Error!",
-                text: `${error.response.data.message}`,
-                icon: "error",
-            })
-        }
-
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const formValues = {
-            medicine: medicine,
-            dosage: dosage,
-            d_finalization: d_finalization,
-            appointment: Appoint.id
-        }
-        try {
-            const res = await createPrescription(formValues);
-            Swal.fire({
-                title: "Exitoso!",
-                text: `${res.message}`,
-                icon: "success",
-                showConfirmButton: false,
-                timer: 1500
-            }).then(() => {
-                navigation('/Home')
-            })
-        }
-        catch (error) {
             Swal.fire({
                 title: "Error!",
                 text: `${error.response.data.message}`,
@@ -69,7 +26,7 @@ const NewPrescription = () => {
     useEffect(() => {
         fetchAppointments()
     }, [])
-
+    
     return (
         <div className='flex items-center justify-center w-full bg-blue-400 px-6  h-screen'>
             <div className='p-4 sm:p-8 shadow-2xl rounded-3xl bg-white h-fit w-[60rem]' >
@@ -96,17 +53,17 @@ const NewPrescription = () => {
                         )}
                     </div>
                     <div className='flex w-[90rem] h-[35vh] md:h-[50vh] justify-center items-center'>
-                        <form className='flex w-full h-full flex-wrap gap-2 justify-center' onSubmit={handleSubmit}>
+                        <form className='flex w-full h-full flex-wrap gap-2 justify-center'>
                             <span className="w-1/5 text-lg font-bold">Paciente:</span>
                             <p className='w-3/5'>{Appoint.name}</p>
                             <span className="w-1/5 text-lg font-bold">Razón:</span>
                             <p className='w-3/5'>{Appoint.reason}</p>
                             <span className="w-1/5 text-lg font-bold">Medicina:</span>
-                            <input className='w-3/5 h-10 p-1 rounded border-2' name='medicine' value={medicine} onChange={InputChange} />
+                            <input className='w-3/5 h-10 p-1 rounded border-2' name='medicine' />
                             <span className="w-1/5 text-lg font-bold">Dosis:</span>
-                            <input className='w-3/5 h-10 p-1 rounded border-2' name='dosage' value={dosage} onChange={InputChange} />
+                            <input className='w-3/5 h-10 p-1 rounded border-2' name='dosage' />
                             <span className="w-1/5 text-lg font-bold">Ultima dosis:</span>
-                            <input type='date' name='d_finalization' value={d_finalization} onChange={InputChange} className='w-3/5 h-10 p-1 rounded border-2' />
+                            <input type='date' name='d_finalization' className='w-3/5 h-10 p-1 rounded border-2' />
                             <input type="submit" value="Recetar" className="w-1/2 p-2 place-self-end bg-black text-white rounded-xl mt-2 hover:bg-slate-100 hover:text-black transition ease-in-out duration-200 hover:ring-2 hover:ring-black" />
                         </form>
                     </div>
@@ -117,4 +74,4 @@ const NewPrescription = () => {
     );
 };
 
-export default NewPrescription;
+export default CloseAppointment;
