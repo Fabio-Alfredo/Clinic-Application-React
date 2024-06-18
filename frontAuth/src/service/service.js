2c5f497adc2caa7dc85ef2d42fd401556e4dc06d
AlexRobles00052121 <00052121@uca.edu.sv>
2024-06-18 00:48:19 -0600

    fix: resolve problems witn merge in service

--- frontAuth/src/service/service.js
+++ frontAuth/src/service/service.js
@@ -16,7 +16,7 @@ export const register = async (formData) => {
         })
 
         return res.data;
-    } catch(error){
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
@@ -31,7 +31,7 @@ export const login = async (formData) => {
         })
 
         return res.data;
-    } catch(error){
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
@@ -47,7 +47,7 @@ export const getAppointments = async (phase) => {
         })
         return res.data;
 
-    } catch(error){
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
@@ -62,7 +62,7 @@ export const createAppointment = async (formData) => {
             }
         })
         return res.data;
-    } catch(error){
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
@@ -77,7 +77,7 @@ export const appointmentsPending = async () => {
             }
         })
         return res.data;
-    } catch(error){
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
@@ -92,7 +92,7 @@ export const approvedAppointment = async (formData) => {
             }
         })
         return res.data;
-    } catch(error){
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
@@ -116,7 +116,13 @@ export const deniedAppointment = async (id) => {
 export const prescriptionByUser = async (id) => {
     try {
         const res = await axios.get(`http://localhost:8080/api/prescription/clinic/prescriptions?id=${id}`, {
-    } catch(error){
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${getToken()}`,
+            }
+        })
+        return res.data;
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
@@ -131,7 +137,7 @@ export const appointmentSchedule = async (date) => {
             }
         })
         return res.data;
-    } catch(error){
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
@@ -146,37 +152,37 @@ export const getRolesUser = async () => {
             }
         })
         return res.data;
-    }catch(error){
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
 }
 
 export const createHistoric = async (formData) => {
-    try{
-        const res = await axios.post('http://localhost:8080/api/user/record',formData,{
+    try {
+        const res = await axios.post('http://localhost:8080/api/user/record', formData, {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${getToken()}`
             }
         })
         return res.data;
-    }catch(error){
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
 }
 
-export const getUser = async ()=>{
-    try{
-        const res = await axios.get('http://localhost:8080/api/user/data',{
+export const getUser = async () => {
+    try {
+        const res = await axios.get('http://localhost:8080/api/user/data', {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${getToken()}`
             }
         })
         return res.data;
-    }catch(error){
+    } catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error.response;
     }
@@ -216,7 +222,7 @@ export const createPrescription = async (formData) => {
 
 export const updateRole = async (formData) => {
     try {
-        const res = await axios.post(`${BASE_URL}/user/config/user-role`, formData,{
+        const res = await axios.post(`${BASE_URL}/user/config/user-role`, formData, {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${getToken()}`
@@ -227,7 +233,7 @@ export const updateRole = async (formData) => {
     catch (error) {
         console.error('Error al realizar la petición:', error);
         throw error;
-    }  
+    }
 }
 
 export const getApprovedAppointments = async () => {
